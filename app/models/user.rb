20fc66153c4f4be8cf523081ce7e0d725b1b2b67
Ilya Hrytsuk <bypotatoes@gmail.com>
2016-04-13 11:20:46 +0300

    Add link to github profile near the answer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
   has_many :user_answers, dependent: :destroy
 
   def self.create_with_omniauth(auth)
-    create! do |user|
+    user = create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
       user.name = auth['info']['name']
+      user.github_url = auth['info']['urls']['GitHub']
     end
+
+    user.reload
   end
 end
