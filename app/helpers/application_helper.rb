43fd0880a9d2eb9f62863726b80a55d2faf5c371
Valentine Zavadsky <saicheg@gmail.com>
2016-04-20 20:56:16 +0300

    fix app description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def app_description
+
+  def app_description_html
     "Fill in your gem starting with the last letter of the previous word. The last wins 50% discount and #{link_to('most performing', leaderboard_path, target: '_blank')} wins a free ticket to RubyConfBy!"
+  end
+
+  def app_description
+    "Fill in your gem starting with the last letter of the previous word. The last wins 50% discount and most performing wins a free ticket to RubyConfBy!"
   end
 end
