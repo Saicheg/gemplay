7126a87778f5b4544a7b52efff6a3573d33a5803
Ilya Hrytsuk <bypotatoes@gmail.com>
2016-04-22 14:35:33 +0300

    Stop answer accepting after finish date

--- app/channels/user_answer_channel.rb
+++ app/channels/user_answer_channel.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class UserAnswerChannel < ApplicationCable::Channel
+  FINISH_DATETIME = DateTime.new(2016, 3, 22, 15).change(offset: '+0300')
+
   def subscribed
     stream_from 'answer_channel'
   end
@@ -9,9 +11,13 @@ class UserAnswerChannel < ApplicationCable::Channel
   end
 
   def push(data)
-    UserAnswer.create! do |answer|
-      answer.text = data['text']
-      answer.user = current_user
+    if Time.current < FINISH_DATETIME
+      UserAnswer.create! do |answer|
+        answer.text = data['text']
+        answer.user = current_user
+      end
+    else
+      UserErrorsChannel.broadcast_to(current_user, errors: ['Event is finished'])
     end
   rescue ActiveRecord::RecordInvalid => invalid
     UserErrorsChannel.broadcast_to(current_user, errors: invalid.record.errors.full_messages)
